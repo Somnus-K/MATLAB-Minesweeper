a31bf2fa9f63cefde22f85fee4cfbce5ba2c80b5
Max Solomon <39502432+masterofheavys@users.noreply.github.com>
2024-04-19 14:04:20 -0400

    added visual grid and coordinate recognition
    updated assets as and added support to display a grid of fixed size corresponding to the 200px by 200px images, coordinate recognition is also implemented. to see this left click on a tile

--- MineSweeper.m
+++ MineSweeper.m
@@ -1,4 +1,4 @@
-function hw()
+function MineSweeper()
     rows = input('Enter number of rows: ');
     cols = input('Enter number of columns: ');
     num_mines = input('Enter number of mines: ');
@@ -8,6 +8,8 @@ function hw()
     
     % Display the board
     display_board(board);
+
+    display_board_images(board);
 end
 
 function board = generate_minesweeper_board(rows, cols, num_mines)
@@ -53,4 +55,73 @@ end
 
 function display_board(board)
     disp(board);
+end
+
+function display_board_images(board)
+    % Load images
+    im0 = imread('0.png');
+    im1 = imread('1.png');
+    im2 = imread('2.png');
+    im3 = imread('3.png');
+    im4 = imread('4.png');
+    im5 = imread('5.png');
+    im6 = imread('6.png');
+    im7 = imread('7.png');
+    im8 = imread('8.png');
+    imMine = imread('seamine.png');
+
+    im0 = flipdim(im0 ,1); 
+    im1 = flipdim(im1 ,1);
+    im2 = flipdim(im2 ,1); 
+    im3 = flipdim(im3 ,1); 
+    im4 = flipdim(im4 ,1); 
+    im5 = flipdim(im5 ,1); 
+    im6 = flipdim(im6 ,1); 
+    im7 = flipdim(im7 ,1); 
+    im8 = flipdim(im8 ,1); 
+    imMine = flipdim(imMine ,1); 
+
+
+    % Define mapping from board values to images
+    image_map = {'0', im0; '1', im1; '2', im2; '3', im3; '4', im4; '5', im5; '6', im6; '7', im7; '8', im8; '*', imMine};
+
+    % Calculate total size required for the grid
+    [rows, cols] = size(board);
+    image_size = 200;  % Assuming all images are 200px by 200px
+    total_width = cols * image_size;
+    total_height = rows * image_size;
+
+    % Set up the figure window with fixed size
+    fig = figure('Units', 'pixels', 'Position', [100, 100, total_width, total_height], 'Resize', 'off');
+
+    % Create axes to hold images
+    ax = axes('Units', 'pixels', 'Position', [0, 0, total_width, total_height], 'Visible', 'off');
+
+    % Display images
+    for r = rows:-1:1 % Invert row iteration
+        for c = 1:cols
+            % Get the corresponding image for the current cell value
+            current_image = image_map{strcmp(board(r, c), image_map(:, 1)), 2};
+            % Calculate position for the image
+            x_start = (c - 1) * image_size + 1;
+            y_start = (rows - r) * image_size + 1; % Adjusted y-coordinate calculation
+            % Display the image
+            image('CData', current_image, 'XData', [x_start, x_start + image_size - 1], 'YData', [y_start, y_start + image_size - 1], 'Parent', ax);
+        end
+    end
+
+    % Define callback function for click event
+    set(fig, 'WindowButtonDownFcn', @imageClicked);
+
+    function imageClicked(~, ~)
+        % Get mouse click coordinates
+        click_point = get(ax, 'CurrentPoint');
+        x = click_point(1,1);
+        y = click_point(1,2);
+        % Convert coordinates to row and column indices
+        col = ceil(x / image_size);
+        row = rows - ceil(y / image_size) + 1; % Adjusted y-coordinate and row index calculation
+        % Print clicked message
+        disp(['Image clicked at coordinates: ', num2str(row), ',', num2str(col)]);
+    end
 end
